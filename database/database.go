4e757984c6c4dd7c5bcd684fa09dec83437a1a41
Arif Widiasan Subagio <arifw.subagio17@gmail.com>
2022-05-05 21:58:02 +0700

    add config user

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/arifwidiasan/todo-app/config"
+	"github.com/arifwidiasan/todo-app/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(conf config.Config) *gorm.DB {
+
+	conectionString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		conf.DB_USERNAME,
+		conf.DB_PASSWORD,
+		conf.DB_HOST,
+		conf.DB_PORT,
+		conf.DB_NAME,
+	)
+	DB, err := gorm.Open(mysql.Open(conectionString), &gorm.Config{})
+	if err != nil {
+		fmt.Println("error open conection : ", err)
+	}
+
+	DB.AutoMigrate(&model.User{})
+	return DB
+}
