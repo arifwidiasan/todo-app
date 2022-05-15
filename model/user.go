ec31d9780ddb7bbe7ba6ebaa707ccdecd810ea0d
Arif Widiasan Subagio <arifw.subagio17@gmail.com>
2022-05-15 11:00:57 +0700

    add size model user

--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	*gorm.Model
 
-	Username   string   `gorm:"uniqueIndex" json:"username"`
+	Username   string   `gorm:"uniqueIndex,size:30" json:"username"`
 	User_email string   `json:"user_email"`
 	User_pass  string   `json:"user_pass"`
 	Accesses   []Access `gorm:"ForeignKey:UserID" json:"accesses"`
